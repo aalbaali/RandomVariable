be7c732940cb78b738cf38c1eadf581ae873cf66
aalbaali <amro.al-baali@mail.mcgill.ca>
2021-03-16 23:06:10 -0400

    Changed filenames, namespaces, added functions from the KF, etc.

--- cpp/example.cpp
+++ cpp/example.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "data_parser.h"
-#include "Exporter.h"
+#include "Eigen/Dense"
+#include "RV.h"
+
 
 int main(int argc, char *argv[]){
-    std::string file_name = "/home/aalbaali/Documents/Code_base/Examples/Data_generator/linear_system/data/msd_ground_truth.txt";
+    std::string file_name = "/home/aalbaali/Documents/Data/Data_generator/linear_system/msd_ground_truth.txt";
 
     std::cout << "Filename: " << file_name << std::endl;
-    auto data = importData( file_name);
+    auto data = RV::IO::read( file_name);
     
     // ************************************************************
     // Go over data vector and display results
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
+# RV IO
+RV IO stands for Random Variables Input/Output. It contains MATLAB and C++ files that write and read realization of random variables as well as their covariance.
+
 # What's in this repo?
 - Mathematial data generator that simulates data from mathematical models.
 - The measurements are corrupted with noise and bias.
+- C++ read/write functions.
+
+# Library and namespace
+The library is stored in the `RV.h` header file. It contains two (nested) namespaces:
+1. `RV` for all functions converning random variables.
+   1. `IO` for input/output operations (such as writing, reading, importing, etc.).
