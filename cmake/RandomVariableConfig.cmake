7c99b79cd1191cad470a88d5ae5c1f9f1fb80967
albaalia <albaalia@live.com>
2021-04-24 01:13:56 -0400

    Changed Config.cmake to make package relocatable

--- cmake/RandomVariableConfig.cmake
+++ cmake/RandomVariableConfig.cmake
@@ -4,10 +4,21 @@ if( TARGET RandomVariable::RandomVariable)
 endif()
 
 add_library(RandomVariable::RandomVariable INTERFACE IMPORTED)
+# Get parent path (project_path)
+# New in version 3.20: 
+#   https://cmake.org/cmake/help/latest/command/cmake_path.html#command:cmake_path
+if( CMAKE_VERSION VERSION_GREATER_EQUAL 3.20)
+    message(DEBUG "Using 'cmake_path' to extract parent directory")
+    cmake_path( GET         CMAKE_CURRENT_LIST_DIR 
+                PARENT_PATH CMAKE_PARENT_CURRENT_LIST_DIR)
+else()                
+    get_filename_component(CMAKE_PARENT_CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
+endif()
+            
 target_include_directories(
     RandomVariable::RandomVariable
     INTERFACE 
-    "${CMAKE_SOURCE_DIR}/include/RandomVariable"
+    "${CMAKE_PARENT_CURRENT_LIST_DIR}/include/RandomVariable"
 )
 
 set_target_properties(
