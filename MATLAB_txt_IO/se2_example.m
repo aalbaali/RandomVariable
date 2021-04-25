5c5e47d538b83e7ca7973dd72c5d6cc63925c356
albaalia <albaalia@live.com>
2021-04-24 23:18:05 -0400

    Renamed se2_example.m

--- MATLAB_txt_IO/se2_example.m
+++ MATLAB_txt_IO/se2_example.m
@@ -26,14 +26,24 @@ idx_range = 1 : min( length(struct_gt.time), length( struct_est.time));
 renormalize = @(X_k) se2alg.expMap( se2alg.vee( SE2.logMap( X_k)));
 X_hat = struct_est.values3d;
 X_gt  = struct_gt.values3d;
+% Create array of StateSE2 objects
+K = size( X_hat, 3);
+X_hat_states( K) = StateSE2();
+X_gt_states( K)  = StateSE2();
+
 for kk = 1 : length( idx_range)
     C_k = X_hat( 1 : 2, 1 : 2, kk);
     C_k = SO2.synthesize( atan2( C_k(2, 1), C_k(1, 1)));
     X_hat( 1 : 2, 1 : 2, kk) = C_k;
+    % StateSE2 objects
+    X_hat_states( kk).state = X_hat( :, :, kk);
+    X_hat_states( kk).time  = struct_est.time( kk);
     
     C_k = X_gt( 1 : 2, 1 : 2, kk);
     C_k = SO2.synthesize( atan2( C_k(2, 1), C_k(1, 1)));
     X_gt( 1 : 2, 1 : 2, kk) = C_k;
+    X_gt_states( kk).state = X_gt( :, :, kk);
+    X_gt_states( kk).time  = struct_est.time( kk);
 end
 %% Plot
 % Lambda function to compute error
@@ -57,7 +67,19 @@ for kk = 1 : 3
         'LineWidth', 1.5);
     plot(  time, -3 * sqrt( squeeze( P( kk, kk, idx_range))), '-.r',...
         'LineWidth', 1.5);
-    ylabel( sprintf('$\\delta \\xi_{%i}$ [m]', kk),...
+    if kk == 1
+        ylabel( sprintf('$\\delta \\xi_{%i}$ [rad]', kk),...
+                        'Interpreter', 'latex', 'FontSize', 14);
+    else
+        ylabel( sprintf('$\\delta \\xi_{%i}$ [m]', kk),...
                     'Interpreter', 'latex', 'FontSize', 14);
+    end
 end
 xlabel( '$t_{k}$ [s]', 'Interpreter', 'latex', 'FontSize', 14);
+
+% Plot trajectory
+figure; 
+plotMlgPose( X_gt_states, '-', matlabColors('orange'));
+hold on;
+plotMlgPose( X_hat_states, '-.', matlabColors('blue'));
+legend({'Ground truth', 'Estimated states'}, 'Interpreter', 'latex', 'FontSize', 14);
